760a70f0cc3058e4bfa4a2f6f973044da149a5d6
Rajveer Singh Saggu <93126793+Rajveer2009@users.noreply.github.com>
2023-03-22 16:44:54 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const ytdl = require('ytdl-core');
+const fs = require('fs');
+const { exec } = require("child_process");
+const { dirname } = require("path");
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+const app = express();
+app.use(express.urlencoded({
+    extended: true
+}))
+
+app.get("/", function (req, res) {
+    res.sendFile(__dirname + "/index.html");
+})
+
+app.post("/download", async (req, res) => {
+    let videoURL = req.body.url
+    let format = req.body.format
+
+    if (format == "video") {
+        ytdl(videoURL, { filter: 'audioandvideo' })
+            .pipe(fs.createWriteStream(`${__dirname}/static/video.webm`))
+            .on('finish', async () => {
+                res.download(`${__dirname}/static/video.webm`)
+                await new Promise(r => setTimeout(r, 5000));
+                exec(`rm ${__dirname}/static/video.webm`)
+            })
+    }
+    else {
+        ytdl(videoURL, { filter: 'audioonly' })
+            .pipe(fs.createWriteStream(`${__dirname}/static/audio.webm`))
+            .on('finish', async () => {
+                res.download(`${__dirname}/static/audio.webm`)
+                await new Promise(r => setTimeout(r, 5000));
+                exec(`rm ${__dirname}/static/audio.webm`)
+            })
+    }
+})
+
+app.listen(8000, function () {
+    console.log("server is running on port http://127.0.0.1:8000/");
+})
